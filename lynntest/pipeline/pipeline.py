ee7d98cba0330598e04644ab3cebbe9ef0b07703
lynn-mt <70601631+lynn-mt@users.noreply.github.com>
2026-01-15 17:12:44 +0000

    pipeline

---
+++ lynntest/pipeline/pipeline.py
@@ -0,0 +1,9 @@
+import sys
+import pandas as pd
+print('argument',sys.argv)
+month = sys.argv[1]
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+df['month'] = month
+df.to_parquet(f"output_{month}.parquet")
+print(df.head())
+print(f'hello pipeline, month={month}')
